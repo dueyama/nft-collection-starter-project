fe53512802040b1ea4354c60f00d8fffb173b4bf
dueyama <98719466+dueyama@users.noreply.github.com>
2023-02-02 11:27:12 +0900

    Update App.js
    end 4-1

--- src/App.js
+++ src/App.js
@@ -39,6 +39,10 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      // **** イベントリスナーをここで設定 ****
+      // この時点で、ユーザーはウォレット接続が済んでいます。
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -65,14 +69,48 @@ const App = () => {
        * ウォレットアドレスを currentAccount に紐付けます。
        */
       setCurrentAccount(accounts[0]);
+
+      // **** イベントリスナーをここで設定 ****
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // setupEventListener 関数を定義します。
+  // MyEpicNFT.sol の中で event が　emit された時に、
+  // 情報を受け取ります。
+  const setupEventListener = async () => {
+    const CONTRACT_ADDRESS = "0x26e97cc687c7A9Ba67c5FbfEe55AA4aD65A0F3FE";
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        // NFT が発行されます。
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+        // Event が　emit される際に、コントラクトから送信される情報を受け取っています。
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `あなたのウォレットに NFT を送信しました。OpenSea に表示されるまで最大で10分かかることがあります。NFT へのリンクはこちらです: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS =
-      "0x0bCe31C7675cdd45Bd7F0e643631091F3A82d90f";
+    const CONTRACT_ADDRESS = "0x26e97cc687c7A9Ba67c5FbfEe55AA4aD65A0F3FE";
     try {
       const { ethereum } = window;
       if (ethereum) {
@@ -87,7 +125,7 @@ const App = () => {
         let nftTxn = await connectedContract.makeAnEpicNFT();
         console.log("Mining...please wait.");
         await nftTxn.wait();
-  
+
         console.log(
           `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
         );
@@ -126,7 +164,10 @@ const App = () => {
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+            <button
+              onClick={askContractToMintNft}
+              className="cta-button connect-wallet-button"
+            >
               Mint NFT
             </button>
           )}
