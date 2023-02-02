0062be7ff6ada52d9a9c86e33e0f7ead42fc3513
dueyama <98719466+dueyama@users.noreply.github.com>
2023-02-02 12:02:24 +0900

    add twitter account

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 // Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
 const TWITTER_HANDLE = "あなたのTwitterのハンドルネームを貼り付けてください";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_LINK = `https://twitter.com/${dueyama}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 const App = () => {
